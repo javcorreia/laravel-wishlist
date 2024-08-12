ecb67ff3b41a3bb924f1c44f9208d18a3dccb87a
José Correia <javcorreia@gmail.com>
2024-08-12 10:39:21 +0100

    more tests: 52% coverage globally, 44% on the wishlist class

--- tests/Unit/WishlistTest.php
+++ tests/Unit/WishlistTest.php
@@ -23,3 +23,20 @@ test('check for wishlist add', function () {
         ->and($checkForRecord->item_id)->toBe(1)
         ->and($countAfter)->toBeGreaterThanOrEqual($countBefore);
 });
+
+test('check for wishlist collection get', function () {
+    $wishlistCollection = \javcorreia\Wishlist\Facades\Wishlist::getUserWishList(1);
+
+    expect($wishlistCollection)->toBeInstanceOf(\Illuminate\Database\Eloquent\Collection::class);
+});
+
+test('check for wishlist remove', function () {
+    $countBefore = \Illuminate\Support\Facades\DB::table('wishlist')->count();
+    $getRecordToRemove = \Illuminate\Support\Facades\DB::table('wishlist')->orderBy('id', 'desc')->first();
+    $wishlistRemoval = \javcorreia\Wishlist\Facades\Wishlist::remove($getRecordToRemove->id, $getRecordToRemove->user_id);
+    $countAfter = \Illuminate\Support\Facades\DB::table('wishlist')->count();
+
+    expect($wishlistRemoval)->toBeBool()
+        ->and($wishlistRemoval)->toBeTrue()
+        ->and($countBefore)->toBeGreaterThan($countAfter);
+});
